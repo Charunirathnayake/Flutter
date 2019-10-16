ebd6b576aecf924e4ad09d1d5738b2a8f4f5d0ba
Charunirathnayake <charunirathnayake1995@gmail.com>
2019-10-16 22:53:41 +0530

    Learning objectives

---
+++ Flutter/sample_project08/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+void main(){
+  runApp(myapp());
+}
+ class myapp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      title: Notekeeper,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      
+      ),
+      home: ,
+    );
+  }
+
+ }
