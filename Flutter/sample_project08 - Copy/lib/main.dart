c65dce803c2494efe2e597ef67e37be82309c1eb
Charunirathnayake <charunirathnayake1995@gmail.com>
2019-10-18 09:17:59 +0530

    updated

---
+++ Flutter/sample_project08 - Copy/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:sample_project08/screens/Screen.dart';
+void main(){
+  runApp(myapp());
+}
+ class myapp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Notekeeper',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      
+      ),
+      home: NoteList(),
+    );
+  }
+
+ }
